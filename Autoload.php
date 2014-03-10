4160458810142a4bccb821eb5916222b7df43c26
serin <serin212@hotmail.com>
2014-03-10 11:52:32 +0700

    Chang name assets --> JQueryAddressAsset

--- Autoload.php
+++ Autoload.php
@@ -2,12 +2,20 @@
 
 namespace bluezip\yii2\jQueryAddress;
 
+use bluezip\yii2\jQueryAddress\assets;
+
 class Autoload extends \yii\base\widget {
     public function init(){
         parent::init();
         \Yii::setAlias('@jQueryAddress', dirname(__FILE__));
+        $this->registerAsset();
     }
     function run() {
         return "Hello!";
+    }
+
+    private function registerAsset(){
+        $view = $this->getView();
+        JQueryAddressAsset::register($view);
     }
 }
